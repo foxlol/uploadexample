02b04c15a76e5ffc514a732620947455d9dce868
Diógenes Dassan Feijó <foxlol@gmail.com>
2019-02-25 09:28:40 -0300

    Connecting with the api

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
--- frontend/src/components/file-list/index.js
+++ frontend/src/components/file-list/index.js
@@ -4,41 +4,47 @@ import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
 
 import { Container, FileInfo, Preview } from "./styles";
 
-const FileList = () => (
+const FileList = ({ files, onDelete }) => (
   <Container>
-    <li>
-      <FileInfo>
-        <Preview src="http://localhost:3000/files/aa5fb755ad81991b7624c3024dc858bd-praia.jpg" />
+    {files.map(file => (
+      <li key={file.id}>
+        <FileInfo>
+          <Preview src={file.preview} />
+          <div>
+            <strong>{file.name}</strong>
+            <span>
+              {file.readableSize}{" "}
+              {!!file.url && (
+                <button onClick={() => onDelete(file.id)}>Excluir</button>
+              )}
+            </span>
+          </div>
+        </FileInfo>
+
         <div>
-          <strong>praia.jpg</strong>
-          <span>
-            64kb <button onClick={() => {}}>Excluir</button>
-          </span>
+          {!file.uploaded && !file.error && (
+            <CircularProgressBar
+              styles={{
+                root: { width: 24 },
+                path: { stroke: "#7159c1" }
+              }}
+              strokeWidth={10}
+              percentage={file.progress}
+            />
+          )}
+
+          {file.url && (
+            <a href={file.url} target="_blank" rel="noopener noreferrer">
+              <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+            </a>
+          )}
+
+          {file.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+
+          {file.error && <MdError size={24} color="#e57878" />}
         </div>
-      </FileInfo>
-
-      <div>
-        <CircularProgressBar
-          styles={{
-            root: { width: 24 },
-            path: { stroke: "#7159c1" }
-          }}
-          strokeWidth={10}
-          percentage={60}
-        />
-
-        <a
-          href="http://localhost:3000/files/aa5fb755ad81991b7624c3024dc858bd-praia.jpg"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-        </a>
-
-        <MdCheckCircle size={24} color="#78e5d5" />
-        <MdError size={24} color="#e57878" />
-      </div>
-    </li>
+      </li>
+    ))}
   </Container>
 );
 
